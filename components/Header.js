3a21fd056fcd1359358c5f63b944be46755dbd31
Imoh Precious <jaypee88830@gmail.com>
2021-12-20 01:36:09 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+import CartSummary from "./CartSummary";
+import header from '../style/header.module.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import LogoSVG from "../svg/logo.svg";
+
+function Header() {
+  return (
+    
+    <>
+      <nav className="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+
+      <header className="md:absolute md:left-0 md:top-0 w-full z-10">
+
+        <div className="py-3 lg:py-5 flex items-center">
+          <Link href="/">
+            <a title="Return to ChopChop">Shop</a>
+          </Link>
+          <span className="pr-1">,</span>
+          hey there
+          <CartSummary />
+        </div>
+
+        <Link href="/">
+          <a title="Return to ChopChop">
+            <LogoSVG className="w-full" />
+          </a>
+        </Link>
+
+      </header>
+    </>
+    
+  );
+}
+
+export default Header;
---
+++ next.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  images: {
+    domains: ["cdn.chec.io"],
+  },
+};
