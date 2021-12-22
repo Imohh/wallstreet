49960c43c6ddb348ecec98924306d0e67b0658c9
Imoh Precious <jaypee88830@gmail.com>
2021-12-22 04:03:55 +0100

    new pages

--- pages/#/fashion.js
+++ pages/#/fashion.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function About() {
   return (
     <>
-      <h1>About Post</h1>
+      <h1>Fashion Page</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
