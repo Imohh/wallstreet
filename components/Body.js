497640ca260af983adf81cea15899eaef408199c
Imoh Precious <jaypee88830@gmail.com>
2021-12-24 02:51:29 +0100

    home page content

--- components/Body.js
+++ components/Body.js
@@ -6,17 +6,18 @@ function Body() {
       <div className="home-page">
 
 
+        {/*TOP IMAGE SECTION*/}
         <div className="container top-section wrapper" style={{backgroundImage: `url("./home-page/bored-ape.png")`}}>
           <div className="row text-overlay">
             <div className="col-lg-12 header">
               {/*<img className="landing-page-img" id="logo" src={"./home-page/bored-ape.png"}/>*/}
               <h1 className="welcome-text">Welcome to wallstreet don</h1>
-              <button className="home-btn welcome-text">Explore</button>
+              <button className="home-btn btn ">Explore</button>
             </div>
           </div>
         </div>
 
-
+      {/*SECTION SECTION(PLAIN TEXT)*/}
 
 
 
