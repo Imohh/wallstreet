36607438b6b4baaf5507cf8bcbd15612c204f47b
Imoh Precious <jaypee88830@gmail.com>
2021-12-24 01:23:37 +0100

    about us title

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
+import Head from "next/head";
 import Header from "../../components/Header"
 
 export default function about() {
   return (
     <>
-    <Header />
+      <Head>
+        <title>Wallstreet Don - About Us</title>
+      </Head>
+
+      <Header />
       <h1>About Post from about</h1>
       <h2>
         <Link href="/">
