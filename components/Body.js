734f55208de1d38ba44c46bbe71e526e9dcccf03
Imoh Precious <jaypee88830@gmail.com>
2021-12-24 02:28:16 +0100

    home page content

--- components/Body.js
+++ components/Body.js
@@ -6,12 +6,12 @@ function Body() {
       <div className="home-page">
 
 
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <img className="landing-page-img" src={"./home-page/bored-ape.png"}/>
-              <h1>Welcome to wallstreet don</h1>
-              <button className="home-btn">Explore</button>
+        <div className="container top-section wrapper" style={{backgroundImage: `url("./home-page/bored-ape.png")`}}>
+          <div className="row text-overlay">
+            <div className="col-lg-12 header">
+              {/*<img className="landing-page-img" id="logo" src={"./home-page/bored-ape.png"}/>*/}
+              <h1 className="welcome-text">Welcome to wallstreet don</h1>
+              <button className="home-btn welcome-text">Explore</button>
             </div>
           </div>
         </div>
