abca5c85aa66c7a777fd4b88ff8dd7d51ae53aa7
Imoh Precious <jaypee88830@gmail.com>
2021-12-24 01:48:37 +0100

    adding content to home page

--- components/Body.js
+++ components/Body.js
@@ -5,11 +5,13 @@ function Body() {
     <>
       <div className="home-page">
 
-      
+
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
               <img className="landing-page-img" src={"./home-page/bored-ape.png"}/>
+              <h1>Welcome to wallstreet don</h1>
+              <button className="home-btn">Explore</button>
             </div>
           </div>
         </div>
