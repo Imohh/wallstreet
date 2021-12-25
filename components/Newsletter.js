f467566c58f1ff788d98eba2aab9e3adcf95689c
Imoh Precious <jaypee88830@gmail.com>
2021-12-26 00:50:05 +0100

    newsletter

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -4,6 +4,7 @@ function Newsletter() {
 			<div className="container newsletter">
 				<div className="row">
 					<div className="col-lg-12">
+					<h2>join our newsletter!</h2>
 						<input type="text" name="newsletter" />
 						<button className="btn">submit</button>
 					</div>
