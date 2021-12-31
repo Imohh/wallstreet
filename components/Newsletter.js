c8592f13c13bc85662d1eeff36dd91f313cf2151
Imoh Precious <jaypee88830@gmail.com>
2021-12-31 10:56:31 +0100

    newsletter wrapper

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -5,8 +5,10 @@ function Newsletter() {
 				<div className="row">
 					<div className="col-lg-12">
 					<h2>join our newsletter!</h2>
-						<input type="text" name="newsletter" />
-						<input type="submit" value="submit" />
+						<form className="newsletter-wrapper">
+							<input type="text" name="newsletter" />
+							<input type="submit" value="submit" />
+						</form>
 					</div>
 				</div>
 			</div>
