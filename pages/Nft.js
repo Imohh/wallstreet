767aa01995eef296acd87ddf9eab018280d61406
Imoh Precious <jaypee88830@gmail.com>
2021-12-31 18:23:59 +0100

    nft page

--- pages/Nft.js
+++ pages/Nft.js
@@ -1,35 +1,44 @@
-import Image from 'next/image'
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Header from "../../components/Header"
 
-function Nft() {
-	return (
+export default function Nft() {
+  return (
+    <>
+      <Head>
+        <title>Wallstreet Don - Nfts</title>
+      </Head>
 
-		// FIRST SECTION
+      <Header />
+      <div className="container">
+			<div className="row">
+				<div className="col-lg-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum
+					</p>
+				</div>
 
-			<div className="container">
-				<div className="row">
-					<div className="col-lg-6">
+				<div className="col-lg-6">
 
+					<div className="col-lg-4">
+						<img src={"test-image.jpg"} />
 					</div>
-
+					<div className="col-lg-4">
+						<img src={"test-image.jpg"} />
+					</div>
 					<div className="col-lg-6">
-
-						<div className="col-lg-4">
-							<img src={"test-image.jpg"} />
-						</div>
-						<div className="col-lg-4">
-							<img src={"test-image.jpg"} />
-						</div>
-						<div className="col-lg-6">
-							<img src={"test-image.jpg"} />
-						</div>
-						<div className="col-lg-6">
-							<img src={"test-image.jpg"} />
-						</div>
+						<img src={"test-image.jpg"} />
+					</div>
+					<div className="col-lg-6">
+						<img src={"test-image.jpg"} />
 					</div>
 				</div>
 			</div>
-
-	)
+		</div>
+    </>
+  )
 }
-
-export default Nft
