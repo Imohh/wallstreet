af06c105dcbb1e8652b9c64637cdc7b9518c6016
Imoh Precious <jaypee88830@gmail.com>
2022-01-01 21:19:49 +0100

    new commit

--- pages/Nft.js
+++ pages/Nft.js
@@ -12,37 +12,38 @@ export default function Nft() {
 
       <Header />
       <div className="first-nft-section">
-    	<div className="container">
-			<div className="row">
-				<div className="col-lg-6">
-				<h2>Welcome to walstreet don</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-					anim id est laborum
-					</p>
-				</div>
+	    	<div className="container">
+					<div className="row">
+						<div className="col-lg-6">
+							<h2>Welcome to walstreet don</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+								ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+								nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+								anim id est laborum
+								</p>
+						</div>
 
-				<div className="col-lg-6">
-					<div className="row rows">
 						<div className="col-lg-6">
-							<img src={"test-image.jpg"} width={150} height={150} />
+							<div className="row rows">
+								<div className="col-lg-6">
+									<img src={"test-image.jpg"} width={150} height={150} />
+								</div>
+								<div className="col-lg-6">
+									<img src={"test-image.jpg"} width={150} height={150} />
+								</div>
+								<div className="col-lg-6">
+									<img src={"test-image.jpg"} width={150} height={150} />
+								</div>
+								<div className="col-lg-6">
+									<img src={"test-image.jpg"} width={150} height={150} />
+								</div>
+							</div>
 						</div>
-						<div className="col-lg-6">
-							<img src={"test-image.jpg"} width={150} height={150} />
-						</div>
-						<div className="col-lg-6">
-							<img src={"test-image.jpg"} width={150} height={150} />
-						</div>
-						<div className="col-lg-6">
-							<img src={"test-image.jpg"} width={150} height={150} />
-						</div>
+
 					</div>
 				</div>
-			</div>
-		</div>
-      </div>
+	    </div>
       
     </>
   )
