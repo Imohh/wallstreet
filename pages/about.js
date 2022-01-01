58f3e60bc2ec2d13252b7bbe385dbdae1c41add7
Imoh Precious <jaypee88830@gmail.com>
2022-01-01 21:41:58 +0100

    header link on about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import Head from "next/head";
-import Header from "../../components/Header"
+import Header from "../components/Header"
 
 export default function about() {
   return (
