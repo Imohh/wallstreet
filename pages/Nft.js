2fde31f343d1ec3e5e0adc1ad3ceba55619e3fff
Imoh Precious <jaypee88830@gmail.com>
2022-01-01 21:12:23 +0100

    nft page

--- pages/Nft.js
+++ pages/Nft.js
@@ -15,6 +15,7 @@ export default function Nft() {
     	<div className="container">
 			<div className="row">
 				<div className="col-lg-6">
+				<h2>Welcome to walstreet don</h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
 					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
 					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
