2025d09932a8c95ca8e80a1a9a4ab4a3de5e2c52
Imoh Precious <jaypee88830@gmail.com>
2022-01-01 21:28:27 +0100

    new commit

--- pages/Nft.js
+++ pages/Nft.js
@@ -51,7 +51,7 @@ export default function Nft() {
 	    	<div className="container">
 					<div className="row">
 						<div className="col-lg-6">
-							<img src={"test-image.jpg"}  />
+							<img src={"test-image.jpg"} />
 						</div>
 
 						<div className="col-lg-6">
@@ -64,12 +64,26 @@ export default function Nft() {
 								</p>
 						</div>
 
-						
-
 					</div>
 				</div>
 	    </div>
-      
+
+		  {/*THIRD SECTION*/}
+      <div className="container">
+      	<div className="row">
+      		<div className="col-lg-12">
+      			<h1>welcome to the club</h1>
+      			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+							ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						</p>
+      		</div>
+      	</div>
+      </div>
+
+
+
     </>
   )
 }
