7bfc9bbe03b3d115e03f71c332cde28aa9974588
Imoh Precious <jaypee88830@gmail.com>
2022-01-03 23:46:20 +0100

    color styling

--- pages/products/[permalink].js
+++ pages/products/[permalink].js
@@ -131,14 +131,14 @@ function ProductPage({ product }) {
               }}
               exit={{ opacity: 0, y: -50 }}
             >
-              <h1 className="font-serif font-medium italic text-2xl md:text-4xl lg:text-5xl">
+              <h1 className="product-name font-serif font-medium italic text-2xl md:text-4xl lg:text-5xl">
                 {product.name}
               </h1>
 
               <div className="flex items-center justify-between pt-3">
                 <div className="flex items-center">
                   <div className="pr-2">
-                    <p className="text-lg md:text-xl lg:text-2xl font-sans">
+                    <p className="product-price text-lg md:text-xl lg:text-2xl font-sans">
                       {product.price.formatted_with_symbol}
                     </p>
                   </div>
@@ -154,7 +154,7 @@ function ProductPage({ product }) {
               </div>
 
               <div
-                className="pt-5 md:pt-8 lg:pt-10 md:leading-relaxed lg:leading-loose lg:text-lg"
+                className="product-description pt-5 md:pt-8 lg:pt-10 md:leading-relaxed lg:leading-loose lg:text-lg"
                 dangerouslySetInnerHTML={{ __html: product.description }}
               />
             </motion.div>
