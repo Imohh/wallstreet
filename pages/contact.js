9d4b87b5f4f501c8de2acbcdc77a45f9b19db52c
Imoh Precious <jaypee88830@gmail.com>
2022-01-09 02:53:20 +0100

    contact page

---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Header from "../components/Header"
+
+export default function contact() {
+  return (
+    <>
+      <Head>
+        <title>Wallstreet Don - Contact Us</title>
+      </Head>
+
+      <Header />
+    </>
+  )
+}
