5be113be43b430f2e3a572ff50bccf89d7bce710
Imoh Precious <jaypee88830@gmail.com>
2022-01-09 02:52:01 +0100

    terms and conditions

---
+++ pages/terms_and_conditions.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Header from "../components/Header"
+
+export default function termsandconditions() {
+  return (
+    <>
+      <Head>
+        <title>Wallstreet Don - Terms and Conditions</title>
+      </Head>
+
+      <Header />
+    </>
+  )
+}
