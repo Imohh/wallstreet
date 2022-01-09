a69327bf106f7a82c4cbafcb45f1ddb4b036f059
Imoh Precious <jaypee88830@gmail.com>
2022-01-09 02:51:13 +0100

    orders and shipping

---
+++ pages/orders_and_shipping.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Header from "../components/Header"
+
+export default function ordersandshipping() {
+  return (
+    <>
+      <Head>
+        <title>Wallstreet Don - Orders and Shipping</title>
+      </Head>
+
+      <Header />
+    </>
+  )
+}
