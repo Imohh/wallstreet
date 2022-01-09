9c15ab2f47b44a5fed4b026064142942f7d3ffe6
Imoh Precious <jaypee88830@gmail.com>
2022-01-09 02:49:38 +0100

    faq page

---
+++ pages/faq.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Header from "../components/Header"
+
+export default function faq() {
+  return (
+    <>
+      <Head>
+        <title>Wallstreet Don - About Us</title>
+      </Head>
+
+      <Header />
+    </>
+  )
+}
