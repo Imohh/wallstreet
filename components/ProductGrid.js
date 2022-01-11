75555ee897f822fbdbc031765c0933dbc7c54612
Imoh Precious <jaypee88830@gmail.com>
2022-01-11 13:33:38 +0100

    new commit

--- components/ProductGrid.js
+++ components/ProductGrid.js
@@ -4,11 +4,25 @@ function ProductGrid({ products, ...props }) {
   if (!products || products.length === 0) return null;
 
   return (
-    <div className="w-full grid lg:grid-cols-2 gap-4 xl:gap-8">
-      {products.map((product) => (
-        <Product key={product.id} {...product} {...props} />
-      ))}
-    </div>
+    <div className="w-full grid lg:grid-cols-2 ">
+       {products.map((product) => (
+         <Product key={product.id} {...product} {...props} />
+       ))}
+     </div>
+
+
+
+    //<>
+    //<div className="container">
+    //  <div className="row">
+    //    <div className="col-lg-4">
+    //      {products.map((product) => (
+    //        <Product key={product.id} {...product} {...props} />
+    //       ))}
+    //    </div>
+    //  </div>
+    //</div>
+    //</>
   );
 }
 
